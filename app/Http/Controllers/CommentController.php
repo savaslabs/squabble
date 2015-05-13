79a5c220f928c1d853099db85aa22407b7916d93
Kosta Harlan <kosta@kostaharlan.net>
2015-05-13 10:40:25 -0400

    Add formatData() method, comments count route

--- app/Http/Controllers/CommentController.php
+++ app/Http/Controllers/CommentController.php
@@ -3,41 +3,60 @@
 namespace App\Http\Controllers;
 
 use App\Comment;
-use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 
 class CommentController extends Controller{
 
-
     public function index() {
-        return Comment::all();
+        $comments = Comment::all();
+        return ($comments) ? $this->formatData($comments) : $this->formatData(array(), FALSE);
     }
 
     public function getComment($id){
-        return Comment::find($id);
+        $comment = Comment::find($id);
+        return ($comment) ? $this->formatData($comment) : $this->formatData(array(), FALSE);
     }
 
     public function getCommentsForPost($slug) {
-        $slug = urldecode($slug);
-        $comments = Comment::all();
-        $commentsForPost = [];
-        foreach ($comments as $comment) {
-            if ($comment['slug'] == $slug) {
-                $commentsForPost[] = $comment;
-            }
-        }
-        return $commentsForPost;
+        $comment = Comment::where('slug', urldecode($slug))->get();
+        return ($comment) ? $this->formatData($comment) : $this->formatData(array(), FALSE);
     }
 
     public function saveComment(Request $request){
+        // TODO: Sanitize input.
         $commentData = array(
             'name' => $request->input('name'),
-            'title' => 'test',
+            'title' => $request->input('title'),
             'email' => $request->input('email'),
             'comment' => $request->input('comment'),
             'slug' => ltrim($request->input('slug'), '/'),
+            'ip' => $request->getClientIp(),
         );
-        return Comment::create($commentData);
+        return $this->formatData(Comment::create($commentData));
+    }
+
+    public function getCommentsCount() {
+        $comments = Comment::all();
+        if (!$comments) {
+            return $this->formatData(array(), FALSE);
+        }
+        $commentData = [];
+        foreach ($comments as $comment) {
+            if (!isset($commentData[$comment->slug])) {
+                $commentData[$comment->slug] = 1;
+            }
+            else {
+                $commentData[$comment->slug]++;
+            }
+        }
+        return $this->formatData($commentData);
+    }
+
+    protected function formatData($data, $success = TRUE) {
+       return array(
+           'success' => $success,
+           'data' => $data,
+       );
     }
 
 }
